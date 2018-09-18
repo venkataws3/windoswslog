73970ca8ceabf35d24142d6d12f7857b8b48e5f5
venkataws3 <venkataws3@gmail.com>
2018-09-18 15:51:30 -0700

    Delete s3bucketforlogs.tf

--- s3bucketforlogs.tf
+++
@@ -1,7 +0,0 @@
-#using module to create s3 bucket for collecting cloudwatch logs
-
-module "s3_bucket_for_logs" {
-  source = "./s3bucket-module"
-  s3_bucket_name="${var.s3_bucket_name}"
-  s3_aws_region="${var.s3_aws_region}"
-}
